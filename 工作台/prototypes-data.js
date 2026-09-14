0ce4bf43c264bd6d36b1e965d8223b1911d864b6
andylalex <40223281@qq.com>
2026-09-14 15:24:41 +0800

    U14 互动卡片优化：导购更名奢品顾问，改为子类分组(奢品顾问/评价有礼)+每条子类最多5条消息列表样式

--- 工作台/prototypes-data.js
+++ 工作台/prototypes-data.js
@@ -112,7 +112,8 @@ const PROTOTYPES_V03=[
       { "name": "付费品牌馆", "url": "v0.4/U8-品牌主页.html" },
       { "name": "商品详情", "url": "v0.4/U9-商品详情页.html" },
       { "name": "搜品牌", "url": "v0.4/U11-搜索品牌页.html" },
-      { "name": "我的", "url": "v0.4/U13-我的页.html" }
+      { "name": "我的", "url": "v0.4/U13-我的页.html" },
+      { "name": "消息中心", "url": "v0.4/U14-消息中心.html" }
     ],
     "srcdoc": "",
     "status": "review",
